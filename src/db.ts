c68c75407d8b4f4ca970cf9ae77f73de9fc7cda6
enzo-bua <enzobua86@gmail.com>
2023-05-22 23:42:55 -0300

    -

--- src/db.ts
+++ src/db.ts
@@ -6,9 +6,9 @@ import { Password } from "./entities/Password";
 //conexion de base de datos
 export  const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'buabua123',
+  host: 'dpg-chlvid64dad6k5lcpjq0-a',
+  username: 'proyect_medico_user',
+  password: 'adnnZBEEtTJR1SXqq79p5OyjKfHdI0LF',
   port: 5432,
   database: 'proyect-medico',
   entities: [Paciente, Consulta, Password],
